d5316e6e8f5fd929b325fc8f0eedfd15620dff9b
Duban Gomez <dubangomez596@gmail.com>
2024-09-26 16:48:55 -0500

    se crea el proyecto con los ejemplos de los operadores

---
+++ operadoresProject/OperadoresAritmeticos.py
@@ -0,0 +1,17 @@
+# E jemplos utilizando operasdores aritmeticos
+numero1 = int (input( " Ingrese el numero1 " ))
+numero2 = int (input( " Ingrese el numero2 " ))
+
+suma = numero1 + numero2
+resta = numero1 - numero2
+division = numero1 / numero2
+multiplicacion = numero1 * numero2
+modulo = numero1 % numero2
+divisionEntera = numero1 // numero2
+
+print(f"La suma de {numero1} y {numero2} es : ", str(suma))
+print(f"La resta de {numero1} y {numero2} es : ", str(resta))
+print(f"La division de {numero1} y {numero2} es : ", str(division))
+print(f"La multiplicacion de {numero1} y {numero2} es : ", str(multiplicacion))
+print(f"La modulo de {numero1} y {numero2} es : ", str(modulo))
+print(f"La divisionEntera es : ", str(divisionEntera))
---
+++ operadoresProject/OperadoresDeComparacion.py
@@ -0,0 +1,13 @@
+# Ejemplo operadores de comparacion
+m = int(input(" Ingrese el valor de m : "))
+n = int(input(" Ingrese el valor de n : "))
+
+print(f"la igualdad de {m} y {n}  es : {(m == n)}") #Igualdad
+print(m > n) #Mayor que
+print(m < n) #Menor que
+print(m >= n) #Mayor igual que
+print(m <= n) #Menor igual que
+print(m != n) #No igual
+
+
+
